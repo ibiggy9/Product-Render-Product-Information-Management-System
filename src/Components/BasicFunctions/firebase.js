819250549088ac07e2fa89e22658376399c418e3
Ian B <44586707+ibiggy9@users.noreply.github.com>
2023-09-25 20:04:17 -0400

    Update firebase.js

--- src/Components/BasicFunctions/firebase.js
+++ src/Components/BasicFunctions/firebase.js
@@ -9,14 +9,7 @@ import { getDatabase} from 'firebase/database'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB59sMF87uActBUgwMVvXV63Z_giV6GYss",
-  authDomain: "productrendercompany.firebaseapp.com",
-  projectId: "productrendercompany",
-  databaseURL: "https://productrendercompany-default-rtdb.firebaseio.com",
-  storageBucket: "productrendercompany.appspot.com",
-  messagingSenderId: "349295666318",
-  appId: "1:349295666318:web:bff7c9b734a504f4a815df",
-  measurementId: "G-K2210G1EJD"
+<Your Config>
 };
 
 // Initialize Firebase
